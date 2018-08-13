973d5eb1f1edfc0dd1d8fcfcb2b67ccb23d09538
Asia Antczak <aasiaantczak@gmail.com>
2018-08-13 21:11:32 +0100

    README update

--- src/Leap.java
+++ src/Leap.java
@@ -1,13 +1,10 @@
 public class Leap {
 
-
     public boolean isLeap(int year) {
         return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
     }
 
-
     public static void main(String[] args) {
-
     }
 
 }
